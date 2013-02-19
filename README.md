e8c7e2b4f5a119f924b19fe58b4c391299a891a5
wpsmith <t@wpsmith.net>
2013-02-19 14:26:53 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,111 @@
-WP-Font-Awesome
-===============
+#Font Awesome Icons
 
-A plugin to properly incorporate Font Awesome for WordPress
+Contributors: wpsmith
+Tags: icons, font-awesome, font icon, UI, icon font, bootstrap
+Requires at least: 3.0
+Tested up to: 3.5
+Stable tag: 3.0
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+![image](http://f.cl.ly/items/3Y0C0i270N281b0v2Q3y/banner-772x250.png)
+
+
+Enables easy use of the Font Awesome icon font set from within WordPress.  Icons can be inserted using either HTML or a shortcode.
+Font Awesome is a pictographic font set of 249 icons.  The icons are infinitely scalable and screen reader compatible.
+A full list of the 249 Font Awesome icons is available: [http://fortawesome.github.com/Font-Awesome/#all-icons](http://fortawesome.github.com/Font-Awesome/#all-icons)
+
+##Usage Instructions
+
+To use any of the Font Awesome icons on your WordPress site you have two options:
+
+__HTML Option__
+
+All code examples on the Font Awesome site apply: [http://fortawesome.github.com/Font-Awesome/#code](http://fortawesome.github.com/Font-Awesome/#code)
+
+**Examples**
+
+Pencil icon
+
+`<i class="icon-pencil"></i>`
+
+Phone icon
+
+`<i class="icon-phone"></i>`
+
+Chevron left icon
+
+`<i class="icon-chevron-left"></i>`
+
+__Shortcode Option__
+
+Don't want to worry about HTML tags?  You can use a shortcode in your posts, pages and even widgets to display a Font Awesome icon.
+
+The shortcode to use is `[icon name=name-of-icon]`, where name=X is the class of the icon you would like to use.
+
+Pencil icon
+
+`[icon name=icon-pencil]`
+
+Phone icon
+
+`[icon name=icon-phone]`
+
+Chevron left icon
+
+`[icon name=icon-chevron-left]`
+
+
+
+##Installation
+
+1. Upload Font Awesome Icons to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Add shortcode to your posts, pages and even widgets to display a Font Awesome icon.
+
+The shortcode to use is `[icon name=name-of-icon]`, where name=X is the class of the icon you would like to use.
+
+##Release Notes
+
+__Version 3.0__
+
+Updated font and CSS files to Font Awesome 3.0 release.
+
+__Version 1.2__
+
+Added IE7 conditional stylesheet
+
+__Version 1.1__
+
+Created and enabled shortcode
+
+__Version 1.0__
+
+##To Do
+
+*   Add TinyMCE button to allow easy insertation of Font Awesome icon
+
+
+##Author
+
+- [Travis Smith](http://wpsmith.net)
+
+##Credits
+
+ Font Awesome webfont and associated code are licensed under CC BY 3.0 and is a production of @fortaweso_me, by Dave Gandy.
+
+##License
+License:
+    Copyright (C) 2012  Travis Smith
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ plugin.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * Plugin Name: WP Font Awesome
+ * Plugin URI: https://wpsmith.net
+ * Description: Use the Font Awesome icon set within WordPress. Icons can be inserted using either HTML or a shortcode.
+ * Version: 3.0.1
+ * Author: Travis Smith
+ * Author URI: http://wpsmith.net
+ *
+ * License:
+ * Copyright (C) 2012  Travis Smith
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+/**
+ * Font Awesome Class
+ *
+ */
+class WP_Font_Awesome {
+
+	public $handles = array();
+	
+    /**
+     * Constructor Method
+     * 
+     */
+    public function __construct() {
+        add_action( 'plugins_loaded', array( &$this, 'plugins_loaded' ) );
+    }
+	
+	/**
+     * Hooks plugin into all basic
+     * 
+     */
+    public function plugins_loaded() {
+		
+		// Register Styles
+        add_action( 'init', array( $this, 'register_styles' ) );
+        
+		// Enqueue Styles
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+		
+		// Add Shortcodes
+		add_shortcode( 'icon', array( $this, 'shortcode' ) );
+		
+		// Add shortcode to widgets
+        add_filter( 'widget_text', 'do_shortcode' );
+    }
+	
+     /**
+      * Builds style suffix for font awesome CSS files based on WP_DEBUG or SCRIPT_DEBUG
+      * 
+      * @param string $script Base name of file
+      * 
+      * @return string $script Full file name
+      */
+	private function suffix( $script ) {
+	
+		$script  = ( ( defined( 'WP_DEBUG' ) && WP_DEBUG ) || ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ) ? $script . '.css' : $script . '.min.css';
+
+		return $script;
+	}
+	
+	/**
+     * Register Font Awesome
+     * 
+     * Two filters: wp_font_awesome_args & wp_font_awesome_ie7_args
+     */
+    public function register_styles() {
+        global $wp_styles;
+		
+		$args = apply_filters( 'wp_font_awesome_args', $this->get_args( 'font-awesome' ) );
+		wp_register_style( $args['handle'], $args['src'], $args['deps'], $args['ver'], $args['media'] );
+		
+		$args = apply_filters( 'wp_font_awesome_ie7_args', $this->get_args( 'font-awesome-ie7' ) );
+		wp_register_style( $args['handle'], $args['src'], $args['deps'], $args['ver'], $args['media'] );
+        $wp_styles->add_data( $args['handle'], 'conditional', 'lte IE 7' );
+    }
+	
+	/**
+     * Register Font Awesome
+     * 
+     */
+	private function get_args( $handle ) {
+		$this->handles[] = $handle;
+		
+		return array(
+			'handle' => $handle,
+			'src'    => plugins_url( 'lib/css/' . $this->suffix( $handle ), __FILE__  ),
+			'deps'   => array(),
+			'ver'    => '3.0',
+			'media'  => 'all',
+		);
+	}
+	
+	/**
+     * Enqueue Font Awesome
+     * 
+     * Style can be prevented via wp_font_awesome_remove custom field.
+     */
+    public function enqueue_styles() {
+		global $post;
+		foreach( $this->handles as $handle ) {
+			if ( ! get_post_meta( $post->ID, 'wp_font_awesome_remove', true ) )
+				wp_enqueue_style( $handle );
+		}
+		
+    }
+	
+	/**
+     * Font Awesome Shortcode
+     * Use: [icon name="icon-pencil"]
+     */
+    public function shortcode( $atts ) {
+		$this->enqueue_styles();
+		
+        shortcode_atts(
+			array(
+				'name' => 'icon-wrench',
+				'tag'  => 'i',
+			), 
+			$atts
+		);
+		
+        $icon = '<' . $atts['tag'] . ' class="' . $atts['name'] . '">&nbsp;</' . $atts['tag'] . '>';
+
+        return $icon;
+    }
+
+}
+
+// Instantiate
+$_wp_font_awesome = new WP_Font_Awesome();
